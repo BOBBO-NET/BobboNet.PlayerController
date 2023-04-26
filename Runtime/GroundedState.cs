ffa5c00021731a8e197a6cb2a8f43eeceda0ae79
LAPTOP-3BPAUQ9G\Iseei <iseeicyhawkston@gmail.com>
2023-04-26 01:09:45 -0500

    Initial Commit

---
+++ Runtime/GroundedState.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BobboNet.PlayerController
+{
+    public enum GroundedState
+    {
+        Grounded,
+        Sliding,
+        InAir
+    }
+}
